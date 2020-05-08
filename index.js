94ce736a3b091cee508a921086415c6e1d2e12b8
zaeyyd <zayyed.not@gmail.com>
2020-05-08 13:30:55 -0400

    https

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const socketio = require('socket.io');
-const http = require('http');
+const http = require('https');
 const router = require('./router');
 const cors = require('cors')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
