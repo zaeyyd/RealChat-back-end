f7e99965ffd857fd30e6006bbeb85c54d23d8dae
Zayyed <zayyedmansoor@gmail.com>
2020-08-09 17:57:31 -0400

    check in and new media

--- index.js
+++ index.js
@@ -1,79 +1,84 @@
-const express = require('express')
-const socketio = require('socket.io');
-const http = require('http');
-const router = require('./router');
-const cors = require('cors')
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const router = require("./router");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000;
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
-
-app.use(router)
-app.use(cors({origin: '*'}))
+app.use(router);
+app.use(cors({ origin: "*" }));
 
 //all the code will run inside this function
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { user, error } = addUser({ id: socket.id, name, room });
 
-    socket.on('join', ({ name, room }, callback) => {
-        const { user, error } = addUser({ id: socket.id, name, room})
+    console.log("joineddd");
 
-        console.log('joineddd')
+    if (error) return callback(error);
 
-        if(error) return callback(error)
+    socket.join(user.room);
 
-        socket.join(user.room)
+    socket.broadcast
+      .to(user.room)
+      .emit("message", {
+        user: "admin",
+        text: `${user.name} joined ${user.room}`,
+      });
+    socket.emit("message", {
+      user: "admin",
+      text: `welcome to ${user.room} ${user.name}!`,
+    });
 
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} joined ${user.room}`})
-        socket.emit('message', { user: 'admin', text: `welcome to ${user.room} ${user.name}!`  })
-        
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
-        
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    callback();
+  });
 
-        
-        callback()
-    })
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
 
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id)
+    console.log(message);
 
-        console.log(message)
+    io.to(user.room).emit("message", { user: user.name, text: message });
 
-        io.to(user.room).emit('message', { user: user.name, text: message})
+    callback();
+  });
 
-        callback()
-    })
+  socket.on("showMessage", (message, callback) => {
+    const user = getUser(socket.id);
 
-    socket.on('showMessage', (message, callback) => {
-        const user = getUser(socket.id)
+    console.log(message);
 
-        console.log(message)
+    io.to(user.room).emit("liveEdit", { user: user.name, text: message });
 
-        
-            io.to(user.room).emit('liveEdit', { user: user.name, text: message})
+    callback();
+  });
 
-        
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
 
-        callback()
-    })
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} left :(`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
 
-
-    socket.on('disconnect', () => {
-
-        const user = removeUser(socket.id)
-
-
-        if(user){
-            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} left :(` });
-            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-        }
-    })
-})
-
-server.listen(PORT, () => console.log(`Server on port ${PORT}`))
-
+server.listen(PORT, () => console.log(`Server on port ${PORT}`));
